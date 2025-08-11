2bbfa19de6e28966b7928e6c1b86f10af458242d
cosmosjoen <cosmosjeon1108@gmail.com>
2025-08-11 13:25:26 +0900

    chore(twa): add Android project sources (exclude builds/keystore)

---
+++ faxi-twa/app/src/main/java/com/cosmosjeon/faxi/DelegationService.java
@@ -0,0 +1,14 @@
+package com.cosmosjeon.faxi;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
